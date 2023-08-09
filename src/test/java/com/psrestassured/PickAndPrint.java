64ee6d72a2ce1be3349165c31fad5eece8c8f50d
fzabala <S3l3nium>
2023-08-09 17:27:36 -0500

    TAE-001 Initial commit

---
+++ src/test/java/com/psrestassured/PickAndPrint.java
@@ -0,0 +1,33 @@
+package com.psrestassured;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class PickAndPrint {
+
+    public static final String BASE_URL = "https://api.github.com";
+
+    @Test
+    public void peek(){
+        RestAssured.get(BASE_URL)
+                .peek();
+    }
+
+    @Test
+    public void pretyPeek(){
+        RestAssured.get(BASE_URL)
+                .prettyPeek();
+    }
+
+    @Test
+    public void print(){
+        RestAssured.get(BASE_URL)
+                .print();
+    }
+
+    @Test
+    public void pretyPrint(){
+        RestAssured.get(BASE_URL)
+                .prettyPrint();
+    }
+}
