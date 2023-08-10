2ea4c59b4afbe69d52d0eb8f4e48bdfd4df96ad4
fzabala <S3l3nium>
2023-08-09 22:29:21 -0500

    TAE-003 add basic response tests

---
+++ src/test/java/com/psrestassured/BasicResponse.java
@@ -0,0 +1,56 @@
+package com.psrestassured;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class BasicResponse {
+
+    public static final String BASE_URL = "https://api.github.com";
+
+    @Test
+    public void convenienceMethod() {
+        Response response = RestAssured.get(BASE_URL).prettyPeek();
+        assertEquals(response.getStatusCode(), 200);
+//        assertEquals(response.getStatusCode(), 201);
+        assertEquals(response.getContentType(), "application/json; charset=utf-8");
+    }
+
+    @Test
+    public void genericHeader() {
+        Response response = RestAssured.get(BASE_URL);
+        assertEquals(response.getHeader("server"), "GitHub.com");
+        assertEquals(response.getHeader("X-RateLimit-Limit"), "60");
+        assertEquals(Integer.parseInt(response.getHeader("X-RateLimit-Limit")), 60);
+    }
+
+    @Test
+    public void getHeaders() {
+        Response response = RestAssured.get(BASE_URL).prettyPeek();
+        Headers headers = response.getHeaders();
+
+        String val = headers.getValue("date");
+        int size = headers.size();
+        System.out.println("size = " + size);
+        List<Header> list = headers.asList();
+        System.out.println("list = " + list);
+        boolean isPresent = headers.hasHeaderWithName("date");
+        assertTrue(isPresent);
+    }
+
+    @Test
+    public void timeExample(){
+        Response response = RestAssured.get(BASE_URL);
+        System.out.println(response.getTime());
+        System.out.println(response.getTimeIn(TimeUnit.MINUTES));
+    }
+}
+
